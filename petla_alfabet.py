8911cbffbd22e2a6f075d7f9fe8b14ed14d11932
Admin <monika_borowicz@o2.pl>
2022-12-06 23:17:25 +0100

    wtorek 23:00

---
+++ petla_alfabet.py
@@ -0,0 +1,10 @@
+print("Alfabet :")
+for i in range(65, 91):
+    litera = chr(i)
+    print(litera)
+
+print("Alfabet odwrócony:")
+for i in range(90, 64, -1):
+    litera = chr(i)
+    print(litera)
+
---
+++ pętla_przykłady.py
@@ -0,0 +1,17 @@
+zmienna=1
+suma=0
+while zmienna<=100:
+    suma=suma+zmienna
+    print(zmienna)
+    zmienna = zmienna + 1
+print(f"Suma : {suma}")
+
+
+zm=0
+sum=0
+while zm<=100:
+    sum=sum+zm
+    print(zm)
+    zm = zm + 2
+print(f"Suma : {sum}")
+
